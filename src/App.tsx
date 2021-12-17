8148a58d3a1b27936dc1f68c6423406987adb339
Alanna <alannapaiva1@hotmail.com>
2021-12-17 17:24:53 -0300

    Finalize the project's css

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Calculadora</h1>
+        <div className='App-input'> 
+          <input type="number" id="value01" name="value01"/>
+          <input type="number" id="value02" name="value02"/>
+        </div>
+        <div>
+          <button>+</button>
+        </div>
       </header>
+
     </div>
+   
   );
 }
 
